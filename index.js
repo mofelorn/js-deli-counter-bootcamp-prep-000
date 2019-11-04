8fb99e730f26e34a1d109aeea8e2978cbe06a900
Felix Moajdo Jr <mojadof@yahoo.com>
2019-11-04 15:09:26 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -22,14 +22,3 @@ function nowServing(katzDeliLine) {
   }   
 }
 
-function currentLine(line) {
-  if (line.length === 0) {
-    return ("The line is currently empty.")
-  } else 
-      var i = 0
-      var name = []
-      for (i; i < line.length; i++ ) {
-        name.push(` ${i+1}. ${line[i]} `)
-      }
-      return (`The line is currently:${name}`)  
-}
